a9ad82f87f8c6f3d089582d6913d8039c44bacc0
sakayat <sakhyatkabir1@gmail.com>
2025-04-12 10:03:04 +0600

    upated register route

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -26,7 +26,6 @@ export const registerUser = async (req: Request, res: Response) => {
     const token = jwt.sign(
       {
         userId: user._id,
-        role: user.role,
       },
       process.env.JWT_SECRET!,
       {
