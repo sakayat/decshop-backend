576e758eee9f0a6436fb6a26c15cdd5bb6340b49
sakayat <sakhyatkabir1@gmail.com>
2025-04-13 17:04:00 +0600

    added admin update-user-staus route

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -26,3 +26,55 @@ export const getAllUsers = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const updateUserStatus = async (req: Request, res: Response) => {
+  try {
+    const { status } = req.body;
+
+    if (!status) {
+      res.status(404).json({
+        success: false,
+        message: "Invalid status value",
+      });
+      return;
+    }
+
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+      return;
+    }
+
+    if (user.role === "admin") {
+      res.status(403).json({
+        success: false,
+        message: "cannot update admin status",
+      });
+      return;
+    }
+
+    user.status = status;
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      data: {
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role,
+        status: user.status,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "server error",
+    });
+  }
+};
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { authorize, protect } from "../middlewares/auth.middleware";
-import { getAllUsers } from "../controllers/admin.controller";
+import { getAllUsers, updateUserStatus } from "../controllers/admin.controller";
 
 const router = Router();
 
 router.get("/users", protect, authorize("admin"), getAllUsers);
+router.post("/user/:id/update-status", protect, authorize("admin"), updateUserStatus);
 
 export default router;
