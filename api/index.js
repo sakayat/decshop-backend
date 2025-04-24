b6efd9c48916c941c9f0218d8798dadad0eab227
sakayat <sakhyatkabir1@gmail.com>
2025-04-24 14:42:13 +0600

    updated

--- api/index.js
+++ api/index.js
@@ -1,21 +1,8 @@
-import express, { Express } from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import path from "path";
-import connectDB from "./config/database";
-import userRoutes from "./routes/user.routes";
-import sellerRoutes from "./routes/seller.routes";
-import orderRoutes from "./routes/order.routes";
-import adminRoutes from "./routes/admin.routes";
-import categoryRoutes from "./routes/category.routes";
-import productRoutes from "./routes/product.route";
-
-dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-
-connectDB();
 
 app.use(
   cors({
@@ -23,20 +10,11 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(express.json());
 app.use(cookieParser());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-app.use("/api/users", userRoutes);
-app.use("/api/seller", sellerRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/category", categoryRoutes);
-app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "DecShop API is running" });
 });
 
-export default app;
+module.exports = app;
